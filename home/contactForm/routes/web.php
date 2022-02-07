f477a58c46867bf21b704969feac5a603a6f8a3b
syogo <oneweb.s.saito@gmail.com>
2022-02-08 03:15:35 +0900

    new: ルーティング

--- home/contactForm/routes/web.php
+++ home/contactForm/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//入力ページ
+// Route::get('/contact', 'ContactController@index');
+Route::get('/contact', 'App\Http\Controllers\ContactController@index');
+
+//確認ページ
+// Route::post('/contact/confirm', 'ContactController@confirm')->name('contact.confirm');
+Route::get('/contact/confirm', 'App\Http\Controllers\ContactController@confirm')->name('contact.confirm');
+
+//送信完了ページ
+// Route::post('/contact/thanks', 'ContactController@send')->name('contact.send');
+Route::get('/contact/thanks', 'App\Http\Controllers\ContactController@send')->name('contact.send');
